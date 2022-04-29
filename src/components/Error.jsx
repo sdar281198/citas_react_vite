24b1ab6ae09b6242a19bb5f903080b3316648223
sdar281198 <santiago181128@gmail.com>
2022-04-29 17:39:26 -0500

    Primer y ultimo commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+const Error = ({children}) => {
+  return (
+    <div>
+        <p className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded">
+            {children}
+        </p>
+    </div>
+  )
+}
+ export default Error;
